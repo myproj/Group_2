6c04467b3b443dbeb33829828c349871a3d07215
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2017-08-31 23:19:41 +0530

    Reverted back to couts

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,8 @@
-#include <cstdio>
+#include <iostream>
 #include "functions.h"
 
 using namespace std;
 
 void print_hello(){
-    printf("Hello, world\n");
+    cout << "Hello, world\n";
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-#include <cstdio>
+#include <iostream>
 #include "functions.h"
 
 using namespace std;
 
 int main(){
     print_hello();
-    printf("\n");
-    printf("The factorial of 5 is %d\n", factorial(5));
+    cout << "\n";
+    cout << "The factorial of 5 is " <<  factorial(5) << endl;
     return 0;
 }
